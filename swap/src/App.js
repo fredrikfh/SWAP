9ea1544a53207102fa1c4fe157acb7cdc5adceb9
idamatre <ida.matre@gmail.com>
2022-02-10 10:54:12 +0100

    Fjernet ubrukt funksjon

--- swap/src/App.js
+++ swap/src/App.js
@@ -2,12 +2,6 @@ import './style/styles.css';
 import AdCard from './components/AdCard';
 
 function App() {
-  function multipleCards() {
-    for (let i = 0; i < 10; i++) {
-      <AdCard></AdCard>;
-    }
-  }
-
   return <AdCard></AdCard>;
 }
 
