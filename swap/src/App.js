d1e5de3673f4ead2797832efa4d0b612f66a742f
idamatre <ida.matre@gmail.com>
2022-02-10 10:46:41 +0100

    Prettier write

--- swap/src/App.js
+++ swap/src/App.js
@@ -1,17 +1,14 @@
-import './style/styles.css';
-import AdCard from './components/AdCard';
+import "./style/styles.css";
+import AdCard from "./components/AdCard";
 
 function App() {
-
   function multipleCards() {
     for (let i = 0; i < 10; i++) {
-      <AdCard></AdCard>
+      <AdCard></AdCard>;
     }
   }
-  
-  return (
-      <AdCard></AdCard>
-  );
+
+  return <AdCard></AdCard>;
 }
 
 export default App;
--- swap/src/components/AdCard.js
+++ swap/src/components/AdCard.js
@@ -1,42 +1,41 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Chip from '@mui/material/Chip';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Chip from "@mui/material/Chip";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
 function AdCard() {
-    const headline = "overskrift fra database";
-    const description = "her kommer en beskrivelse om annonsen osv heehehhe";
-    const tagsList = ["Til salgs", "Trondheim Spektrum", "4.mai"];
+  const headline = "overskrift fra database";
+  const description = "her kommer en beskrivelse om annonsen osv heehehhe";
+  const tagsList = ["Til salgs", "Trondheim Spektrum", "4.mai"];
 
-    function mapTest() {
-        for (let i = 0; i < tagsList.length; i ++) {
-            console.log(tagsList[i]);
-        }
+  function mapTest() {
+    for (let i = 0; i < tagsList.length; i++) {
+      console.log(tagsList[i]);
     }
-    mapTest();
-    
+  }
+  mapTest();
+
   return (
-    <Card sx={{ maxWidth: 345, 
-    color: 'hotpink'}}>
-    <CardActionArea>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {headline}
-        </Typography>
-        
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-    <CardActions>
-      <Button size="small" color="primary">
-        Send melding
-      </Button>
-    </CardActions>
-  </Card>
+    <Card sx={{ maxWidth: 345, color: "hotpink" }}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {headline}
+          </Typography>
+
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Send melding
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
 
