0d03c7ded6b7247a9bf04b0df4064352fd99a05e
Erlennkr <Erlennkr@stud.ntnu.no>
2022-02-15 13:08:05 +0100

    prettier

--- swap/src/components/AdCard.js
+++ swap/src/components/AdCard.js
@@ -2,11 +2,11 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 AdCard.propTypes = {
-	post: PropTypes.any
-}
+	post: PropTypes.any,
+};
 
 function AdCard(props) {
 	// const headline = "overskrift fra database";
