28f5c45a96703c93b661afca3c722697478c049b
Erlennkr <Erlennkr@stud.ntnu.no>
2022-02-15 13:03:13 +0100

    Prettier og propType

--- swap/src/App.js
+++ swap/src/App.js
@@ -1,26 +1,30 @@
-import './style/styles.css';
+import "./style/styles.css";
 import { useState, useEffect } from "react";
 import AdCard from "./components/AdCard";
-import {db} from "./firebase-config";
+import { db } from "./firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const postsCollectionRef = collection(db, "posts")
-  useEffect(() => {
+	const [posts, setPosts] = useState([]);
+	const postsCollectionRef = collection(db, "posts");
+	useEffect(() => {
+		const getPosts = async () => {
+			const data = await getDocs(postsCollectionRef);
+			setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
 
-    const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
-      setPosts(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
-    }
-
-    getPosts();
-  }, [])
-  return <div>
-    <div className="App"> {posts.map((post, index) => { return <AdCard key={index} post={post}></AdCard>})}
-    </div>
-  </div>;
+		getPosts();
+	}, []);
+	return (
+		<div>
+			<div className="App">
+				{" "}
+				{posts.map((post, index) => {
+					return <AdCard key={index} post={post}></AdCard>;
+				})}
+			</div>
+		</div>
+	);
 }
-
 
 export default App;
--- swap/src/components/AdCard.js
+++ swap/src/components/AdCard.js
@@ -2,11 +2,16 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import PropTypes from 'prop-types';
+
+AdCard.propTypes = {
+	post: PropTypes.any
+}
 
 function AdCard(props) {
-  const headline = 'overskrift fra database';
-  const description = 'her kommer en beskrivelse om annonsen osv heehehhe';
-  const tagsList = ['Til salgs', 'Trondheim Spektrum', '4.mai'];
+	// const headline = "overskrift fra database";
+	// const description = "her kommer en beskrivelse om annonsen osv heehehhe";
+	const tagsList = ["Til salgs", "Trondheim Spektrum", "4.mai"];
 
 	function mapTest() {
 		for (let i = 0; i < tagsList.length; i++) {
@@ -15,26 +20,26 @@ function AdCard(props) {
 	}
 	mapTest();
 
-  return (
-    <Card sx={{ maxWidth: 345, color: 'hotpink' }}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.post.title}
-          </Typography>
+	return (
+		<Card sx={{ maxWidth: 345, color: "hotpink" }}>
+			<CardActionArea>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						{props.post.title}
+					</Typography>
 
-          <Typography variant="body2" color="text.secondary">
-            {props.post.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Send melding
-        </Button>
-      </CardActions>
-    </Card>
-  );
+					<Typography variant="body2" color="text.secondary">
+						{props.post.description}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Button size="small" color="primary">
+					Send melding
+				</Button>
+			</CardActions>
+		</Card>
+	);
 }
 
 export default AdCard;
