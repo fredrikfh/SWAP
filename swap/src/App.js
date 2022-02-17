c469a0c28abcce13a1c4c2ced84a34a3bc0cb43b
Fredrik Fonn Hansen <fredrfha@stud.ntnu.no>
2022-02-17 10:39:14 +0100

    Resolve "Test firebase deploy"

--- swap/src/App.js
+++ swap/src/App.js
@@ -1,39 +1,8 @@
 import "./style/styles.css";
-import React from "react"; /*{ useState, useEffect }*/
-import Navbar from "./components/Navbar";
-import CardOverview from "./pages/CardOverview";
-/*import AdCard from "./components/AdCard";
-import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";*/
+import AdCard from "./components/AdCard";
 
 function App() {
-	/*const [posts, setPosts] = useState([]);
-	const postsCollectionRef = collection(db, "posts");
-	useEffect(() => {
-		const getPosts = async () => {
-			const data = await getDocs(postsCollectionRef);
-			setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-		};
-
-		getPosts();
-	}, []);
-	return (
-		<div>
-			<div className="App">
-				{" "}
-				{posts.map((post, index) => {
-					return <AdCard key={index} post={post}></AdCard>;
-				})}
-			</div>
-		</div>
-	);*/
-
-	return (
-		<React.Fragment>
-			<Navbar />
-			<CardOverview />
-		</React.Fragment>
-	);
+	return <AdCard></AdCard>;
 }
 
 export default App;
