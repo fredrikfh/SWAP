f7fd108493b6f819f1e286e6327aa4d7c8273c81
eastorks <eastorks@stud.ntnu.no>
2022-02-17 13:19:20 +0100

    Installer react-router og lag enkel Router i App

--- swap/src/App.js
+++ swap/src/App.js
@@ -1,33 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CardOverview from "./pages/CardOverview";
 
 export default function App() {
-	/*const [posts, setPosts] = useState([]);
-	const postsCollectionRef = collection(db, "posts");
-	useEffect(() => {
-		const getPosts = async () => {
-			const data = await getDocs(postsCollectionRef);
-			setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-		};
-
-		getPosts();
-	}, []);
-	return (
-		<div>
-			<div className="App">
-				{" "}
-				{posts.map((post, index) => {
-					return <AdCard key={index} post={post}></AdCard>;
-				})}
-			</div>
-		</div>
-	);*/
-
 	return (
 		<React.Fragment>
 			<Navbar />
-			<CardOverview />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<CardOverview />} />
+				</Routes>
+			</BrowserRouter>
 		</React.Fragment>
 	);
 }
