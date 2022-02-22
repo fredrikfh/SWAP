51980b0fc5ccede203c90a58a4aaba1a96534fbd
Erlennkr <Erlennkr@stud.ntnu.no>
2022-02-22 14:00:34 +0100

    Fikk opp kortene

--- swap/src/pages/CardOverview.js
+++ swap/src/pages/CardOverview.js
@@ -3,7 +3,23 @@ import Card from "@mui/material/Card";
 
 import FilterMenu from "../components/FilterMenu";
 
+import AdCard from "../components/AdCard";
+import { useState, useEffect } from "react";
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+
 export default function CardOverview() {
+	const [posts, setPosts] = useState([]);
+	const postsCollectionRef = collection(db, "posts");
+	useEffect(() => {
+		const getPosts = async () => {
+			const data = await getDocs(postsCollectionRef);
+			setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+
+		getPosts();
+	}, []);
+
 	return (
 		/*Hoved-Container*/
 		<Container
@@ -38,6 +54,24 @@ export default function CardOverview() {
 					},
 				}}
 			>
+				<div className="adCards">
+					{posts.map((post, index) => {
+						return <AdCard
+							key={index}
+							post={post}
+							sx={{
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+								width: "calc(100% - 3em)",
+								height: "12em",
+								margin: "1em auto 0 auto",
+								background: "white",
+							}}
+						>
+						</AdCard>;
+					})}
+				</div>
 				<Card
 					sx={{
 						display: "flex",
