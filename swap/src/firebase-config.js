6cc0b6c000797f87d45ef303dcbbc1900300a497
Erlennkr <Erlennkr@stud.ntnu.no>
2022-03-08 13:50:14 +0100

    prettier

--- swap/src/firebase-config.js
+++ swap/src/firebase-config.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
 	apiKey: "AIzaSyAYYC1bYILp1jZnxE_QgAxfMbfJ8WTBMYg",
 	authDomain: "swap-93d9f.firebaseapp.com",
@@ -10,7 +9,7 @@ const firebaseConfig = {
 	projectId: "swap-93d9f",
 	storageBucket: "swap-93d9f.appspot.com",
 	messagingSenderId: "371301076665",
-	appId: "1:371301076665:web:bd7dc9043536ed1b3bd568"
+	appId: "1:371301076665:web:bd7dc9043536ed1b3bd568",
 };
 
 const app = initializeApp(firebaseConfig);
